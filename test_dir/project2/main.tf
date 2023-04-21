737cd976cbc7dc6c1ab86b11e53fc887c5c8757d
Antoine Barthelemy <antoine@linux.com>
2023-04-21 15:41:38 +0200

    test

--- test_dir/project2/main.tf
+++ test_dir/project2/main.tf
@@ -1,8 +1,18 @@
 terraform {
-  required_version = "~> 1.24"
+  required_version = "~> 1.3"
 }
 
-module "conditionally_used_env_module" {
-  source = "./${var.environment}"
+module "conditionally_used_dev_module" {
+  source = "./dev"
+  count  = (var.environment == "dev") ? 1 : 0
 }
 
+module "conditionally_used_staging_module" {
+  source = "./dev"
+  count  = (var.environment == "staging") ? 1 : 0
+}
+
+module "conditionally_used_production_module" {
+  source = "./production"
+  count  = (var.environment == "production") ? 1 : 0
+}
