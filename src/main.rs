f125433622e14409bb818a73f38b9e2a2723ac78
Antoine Barthelemy <antoine@linux.com>
2023-04-24 21:30:23 +0200

    not working

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,16 @@ fn main() -> io::Result<()> {
     for entry in WalkDir::new(current_dir.clone()) {
         let entry = entry?;
         if entry.file_type().is_file()
-            && entry
+            && (entry
                 .path()
                 .extension()
-                .map(|e| e == "tfvars" || e == "tfvars.json")
+                .map(|e| e == "tfvars")
                 .unwrap_or(false)
+                || entry
+                    .path()
+                    .extension()
+                    .map(|e| e == "tfvars.json")
+                    .unwrap_or(false))
         {
             let entry_path = fs::canonicalize(entry.path().display().to_string()).unwrap();
             let relative_path = entry_path.strip_prefix(&current_dir).unwrap();
