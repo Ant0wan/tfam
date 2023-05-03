4c93ead205751ce3333984c6f1d69526b3075b95
Antoine Barthelemy <antoine@linux.com>
2023-05-03 18:10:37 +0200

    adding cli

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,76 @@ use std::fs;
 use std::io;
 use std::process::Command;
 use walkdir::WalkDir;
+use std::env;
+
+pub fn print_usage() {
+    println!("Usage: /usr/bin/taskmasterctl [options] [action [arguments]]");
+    println!("\nOptions:");
+    println!("-c/--configuration FILENAME -- configuration file path (searches if not given)");
+    println!("-h/--help -- print usage message and exit");
+    println!("-i/--interactive -- start an interactive shell after executing commands");
+    println!("-s/--serverurl URL -- URL on which taskmasterd server is listening (default \"http://localhost:9001\")");
+    println!("-u/--username USERNAME -- username to use for authentication with server");
+    println!("-p/--password PASSWORD -- password to use for authentication with server");
+    println!("-r/--history-file -- keep a readline history (if readline is available)");
+    println!("\nActions are commands like \"tail\" or \"stop\". If -i is specified or no action is specified on the command line, a \"shell\" interpreting actions typed interactively is started. Use the action \"help\" to find out about available actions.");
+}
+
+pub struct Args {
+    pub help: bool,
+    pub interactive: bool,
+    pub varfiles: Vec<String>,
+    pub action: Option<String>,
+    pub arguments: Vec<String>,
+}
+
+impl Args {
+    fn new() -> Self {
+        Args {
+            help: false,
+            interactive: false,
+            varfiles: Vec::new(),
+            arguments: Vec::new(),
+        }
+    }
+}
+
+pub fn parse_args() -> Args {
+    let mut args = Args::new();
+
+    let mut args_iter = env::args().skip(1);
+    while let Some(arg) = args_iter.next() {
+        match arg.as_str() {
+            "-h" | "--help" => {
+                args.help = true;
+            }
+            "-i" | "--interactive" => {
+                args.interactive = true; // var-files in cli only
+            }
+            "-var-file" => {
+                args.varfiles = Some(args_iter.next().expect("missing var file")); // should append
+            }
+            _ => {
+                if args.action.is_none() {
+                    args.action = Some(arg);
+                } else {
+                    args.arguments.push(arg);
+                }
+            }
+        }
+    }
+
+    args
+}
+
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let args = parse_args();
+    if args.help {
+        print_usage();
+    }
+
+//    let args: Vec<String> = env::args().skip(1).collect();
 
     if args.len() < 1 {
         let status = Command::new("terraform").status();
