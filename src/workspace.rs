fbfafe1567359bd685c6ee27605d67b53d45d16b
Antoine Barthelemy <antoine@linux.com>
2023-05-08 15:43:04 +0200

    fix extension parsing for workspace naming standard

--- src/workspace.rs
+++ src/workspace.rs
@@ -10,14 +10,7 @@ pub fn get_workspace(path: String) -> String {
 fn convert_path_to_workspace(path: String) -> String {
     let parts: Vec<&str>;
 
-    let without_extension = if path.ends_with(".tfvars.json") {
-        path.trim_end_matches(".tfvars.json").to_string()
-    } else if path.ends_with(".tfvars") {
-        path.trim_end_matches(".tfvars").to_string()
-    } else {
-        path
-    };
-
+    let without_extension = path.splitn(2, '.').next().unwrap_or(&path);
     parts = without_extension.split('/').collect();
     println!("{:?}", parts);
     //    if format.len()
