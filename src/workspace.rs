2540c8319b0495dd4e7698498f2ee6e3e729ae07
Antoine Barthelemy <antoine@linux.com>
2023-05-09 15:38:21 +0200

    hop

--- src/workspace.rs
+++ src/workspace.rs
@@ -23,11 +23,23 @@ fn convert_path_to_workspace(path: String, format: String) -> String {
 }
 
 fn replace_placeholders(vector: Vec<&str>, str_expr: String) -> String {
-    let mut result = String::from(str_expr);
+    let mut result = String::from(str_expr.clone());
 
-    for (i, s) in vector.iter().enumerate() {
+    for i in 0..vector.len() {
         let placeholder = format!("${}", i + 1);
-        result = result.replace(&placeholder, s);
+        let negative_placeholder = format!("$-{}", i + 1);
+        let value = if str_expr.contains(&negative_placeholder) {
+            vector.get(vector.len() - i - 1)
+        } else {
+            vector.get(i)
+        };
+
+        if let Some(s) = value {
+            result = result.replace(&placeholder, s);
+            result = result.replace(&negative_placeholder, s);
+        } else {
+            panic!("Index {} is out of range", i + 1);
+        }
     }
 
     result
