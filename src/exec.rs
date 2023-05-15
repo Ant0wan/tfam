49d3400d162e55d707f136e85bd45e18df5dd6cc
Antoine Barthelemy <antoine@linux.com>
2023-05-15 19:15:17 +0200

    wip

--- src/exec.rs
+++ src/exec.rs
@@ -9,58 +9,58 @@ use std::thread;
 use crate::cli::Commands;
 use crate::workspace::get_workspace;
 
-fn process_file(args: &Vec<String>, cmd: &Commands, f: &String, exe: &mut Command) -> ExitStatus {
-    let workspace = get_workspace(f, &cmd.workspaceformat);
+fn process_file(args: &Vec<String>, cmd: &Commands, file: &String) -> ExitStatus {
+    let workspace = get_workspace(file, &cmd.workspaceformat);
     println!(
         "TF_WORKSPACE={} {} {} -var-file {:?}",
         workspace,
         cmd.bin,
         args.join(" "),
-        f
+        file
     );
-    exe.env("TF_WORKSPACE", workspace)
+    Command::new(&cmd.bin)
+        .args(args)
+        .env("TF_WORKSPACE", workspace)
         .arg("-var-file")
-        .arg(f)
+        .arg(file)
         .status()
         .expect(&format!("failed to execute {}", cmd.bin))
 }
 
 pub fn exec(args: &Vec<String>, cmd: &Commands) -> ExitStatus {
-    let mut exe = Command::new(&cmd.bin);
-    let exe = exe.args(args);
     let mut last_error = ExitStatus::from_raw(0);
 
     match cmd.varfiles.is_empty() {
         true => {
             println!("{} {}", cmd.bin, args.join(" "));
-            exe.status()
+            Command::new(&cmd.bin)
+                .args(args)
+                .status()
                 .expect(&format!("failed to execute {}", cmd.bin))
         }
         false => match cmd.concurrent {
             true => {
-                exe.status()
-                    .expect(&format!("failed to execute {}", cmd.bin))
-                //process_file(args, cmd, f, exe)
-                //       let handles: Vec<_> = cmd
-                //           .varfiles
-                //           .iter()
-                //           .map(|f| {
-                //               thread::spawn(move || {
-                //                   process_file(args, cmd, f, &cmd.bin, exe);
-                //               })
-                //           })
-                //           .collect();
-                //       for handle in handles {
-                //           let _status_result = handle.join().unwrap();
-                //           //if !status_result.success() {
-                //           //    last_error = status_result;
-                //           // }
-                //       }
-                //       last_error
+                let handles: Vec<_> = cmd
+                    .varfiles
+                    .iter()
+                    .map(|f| {
+                        let args = args.clone();
+                        let cmd = cmd.clone();
+
+                        thread::spawn(move || process_file(&args, cmd, &f))
+                    })
+                    .collect();
+                for handle in handles {
+                    let status_result = handle.join().unwrap();
+                    if !status_result.success() {
+                        last_error = status_result;
+                    }
+                }
+                last_error
             }
             false => {
-                for f in &cmd.varfiles {
-                    let status_result = process_file(args, cmd, f, exe);
+                for file in &cmd.varfiles {
+                    let status_result = process_file(args, cmd, file);
                     if !status_result.success() {
                         last_error = status_result;
                     }
