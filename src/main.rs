8dd815baf84211de393798166e7ead3f98ab8a7d
Antoine Barthelemy <antoine@linux.com>
2023-05-27 18:22:35 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use cli::Commands;
 use exec::exec;
 use prompt::select_tfvars_files;
 use std::env;
+use std::path::PathBuf;
 use std::process::exit;
+use std::process::ExitStatus;
 use vars::find_tfvars_files;
 
 pub mod cli;
@@ -15,18 +17,18 @@ pub mod vars;
 pub mod workspace;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = env::args().skip(1).collect();
-    let mut cmd = Commands::parse_commands(args);
+    let args: Vec<String> = env::args().skip(1).collect();
+    let mut cmd: Commands = Commands::parse_commands(args);
     if cmd.help {
         print_usage();
     }
     if cmd.interactive {
-        let current_dir = env::current_dir()?;
-        let mut results = find_tfvars_files(&current_dir)?;
+        let current_dir: PathBuf = env::current_dir()?;
+        let mut results: Vec<String> = find_tfvars_files(&current_dir)?;
         cmd.varfiles.append(&mut results);
         cmd.varfiles = select_tfvars_files(cmd.varfiles).unwrap();
     }
     cmd.varfiles.sort();
-    let exit_status = exec(&cmd);
+    let exit_status: ExitStatus = exec(&cmd);
     exit(exit_status.code().unwrap_or(1));
 }
